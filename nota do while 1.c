01ccfc6ba465c04cc88a97bf1bfce6c3a58f1e02
mrandrenascimento <159596002+mrandrenascimento@users.noreply.github.com>
2024-03-12 11:29:20 -0300

    Add files via upload

---
+++ nota do while 1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+void cabecalho(){
+	setlocale(LC_ALL,"");
+	system("cls\\clear");
+	printf("\n\tSENAI DENDEZEIRO\n");
+	fflush(stdin);
+}
+int main(){
+	int i, soma;
+	float nota, media;
+	
+cabecalho();
+for(i=1;i<=2;i++){
+	
+	do{
+
+	printf("Digite a %iª Nota: ",i);
+	scanf("%f",&nota);
+}
+	while(nota<0||nota>10);
+
+		soma=soma+nota;
+}
+		media=soma/2;
+		printf("Média: %.1f \n",media);
+} 	if (media>=7){
+	printf("Aprovado!");
+}	else if (media>=5){
+	printf("Recuperação")
+}	else{printf("Reprovado")
+}
+	return 0;
+
+}
+
